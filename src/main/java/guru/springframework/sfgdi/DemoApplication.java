ae9e82bf0966806858752758e4e7c6281e64785f
Enol <grimgaiden@gmail.com>
2020-06-25 10:02:50 +0200

    Primary Beans

--- src/main/java/guru/springframework/sfgdi/DemoApplication.java
+++ src/main/java/guru/springframework/sfgdi/DemoApplication.java
@@ -17,9 +17,9 @@ public class DemoApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
+		System.out.println("------Primary Bean------");
 
-		System.out.println((greeting));
+		System.out.println((myController.sayHello()));
 
 		System.out.println("------Property------");
 
